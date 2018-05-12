6c4dc3b583896712a7fa3fa0598decc6280a439f
Thomas Fuller <tomkc518@yahoo.com>
2018-05-12 13:08:43 -0500

    updating backend information

---
+++ public/assets/js/lost.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+  //create a new lost item
+
+  $("#createLost").on("submit", function (event) {
+    //preventDefault on a submit event.
+    event.preventDefault();
+
+    var newLost = {
+      name: $("#searcher_name_input").val().trim(),
+      email: $("#searcher_email_input").val().trim(),
+      phone: $("#searcher_phone_input").val().trim(),
+      item: $("#thing_input").val().trim(),
+      description: $("#description_input").val().trim(),
+      image: $("#image_input").val().trim()
+    };
+    console.log(newLost);
+      // Send the POST request.
+      $.ajax("/api/posts", {
+        type: "POST",
+        data: newLost
+      }).then(
+        function() {
+          // Reload the page to get the updated list
+          $("#lostEntry").modal('toggle');
+      })
+  });
+
+  $("#updateLost").on("submit", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var id = $("[name=id]").val().trim();
+
+    var updatedLost = {
+      lost: $("######").val().trim()
+    };
+
+    // Send the PUT request.
+    $.ajax("#####" + id, {
+      type: "PUT",
+      data: updatedPlan
+    }).then(
+      function () {
+        console.log("updated id ", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
